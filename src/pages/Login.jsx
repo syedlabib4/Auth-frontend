a2b0fceebe89ed5d1381f3e77251fab49dc34ac6
SYED LABIB <124432659+syedlabib4@users.noreply.github.com>
2025-10-04 18:51:25 +0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/auth/login", {
+      const response = await fetch("https://auth-backend-snowy.vercel.app/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(LoginInfo),
