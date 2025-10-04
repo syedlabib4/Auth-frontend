198132f92667558dd5c7533a5042d0aee7c041d2
SYED LABIB <124432659+syedlabib4@users.noreply.github.com>
2025-10-04 18:51:45 +0500

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -25,7 +25,7 @@ const SignUp = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/auth/signup", {
+      const response = await fetch("https://auth-backend-snowy.vercel.app/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(signupInfo),
